913e5a334db5ed741ef8e93a6ed3d8584846f5da
DavSladk <davsladk+github@gmail.com>
2022-11-23 00:22:07 +0100

    Init commit.

---
+++ step0/nbody.cu
@@ -0,0 +1,98 @@
+/**
+ * @File nbody.cu
+ *
+ * Implementation of the N-Body problem
+ *
+ * Paralelní programování na GPU (PCG 2022)
+ * Projekt c. 1 (cuda)
+ * Login: xlogin00
+ */
+
+#include <cmath>
+#include <cfloat>
+#include "nbody.h"
+
+/**
+ * CUDA kernel to calculate gravitation velocity
+ * @param p       - particles
+ * @param tmp_vel - temp array for velocities
+ * @param N       - Number of particles
+ * @param dt      - Size of the time step
+ */
+__global__ void calculate_gravitation_velocity(t_particles p, t_velocities tmp_vel, int N, float dt)
+{
+
+}// end of calculate_gravitation_velocity
+//----------------------------------------------------------------------------------------------------------------------
+
+/**
+ * CUDA kernel to calculate collision velocity
+ * @param p       - particles
+ * @param tmp_vel - temp array for velocities
+ * @param N       - Number of particles
+ * @param dt      - Size of the time step
+ */
+__global__ void calculate_collision_velocity(t_particles p, t_velocities tmp_vel, int N, float dt)
+{
+
+}// end of calculate_collision_velocity
+//----------------------------------------------------------------------------------------------------------------------
+
+/**
+ * CUDA kernel to update particles
+ * @param p       - particles
+ * @param tmp_vel - temp array for velocities
+ * @param N       - Number of particles
+ * @param dt      - Size of the time step
+ */
+__global__ void update_particle(t_particles p, t_velocities tmp_vel, int N, float dt)
+{
+
+}// end of update_particle
+//----------------------------------------------------------------------------------------------------------------------
+
+/**
+ * CUDA kernel to update particles
+ * @param p       - particles
+ * @param comX    - pointer to a center of mass position in X
+ * @param comY    - pointer to a center of mass position in Y
+ * @param comZ    - pointer to a center of mass position in Z
+ * @param comW    - pointer to a center of mass weight
+ * @param lock    - pointer to a user-implemented lock
+ * @param N       - Number of particles
+ */
+__global__ void centerOfMass(t_particles p, float* comX, float* comY, float* comZ, float* comW, int* lock, const int N)
+{
+
+}// end of centerOfMass
+//----------------------------------------------------------------------------------------------------------------------
+
+/**
+ * CPU implementation of the Center of Mass calculation
+ * @param particles - All particles in the system
+ * @param N         - Number of particles
+ */
+__host__ float4 centerOfMassCPU(MemDesc& memDesc)
+{
+  float4 com = {0 ,0, 0, 0};
+
+  for(int i = 0; i < memDesc.getDataSize(); i++)
+  {
+    // Calculate the vector on the line connecting current body and most recent position of center-of-mass
+    const float dx = memDesc.getPosX(i) - com.x;
+    const float dy = memDesc.getPosY(i) - com.y;
+    const float dz = memDesc.getPosZ(i) - com.z;
+
+    // Calculate weight ratio only if at least one particle isn't massless
+    const float dw = ((memDesc.getWeight(i) + com.w) > 0.0f)
+                          ? ( memDesc.getWeight(i) / (memDesc.getWeight(i) + com.w)) : 0.0f;
+
+    // Update position and weight of the center-of-mass according to the weight ration and vector
+    com.x += dx * dw;
+    com.y += dy * dw;
+    com.z += dz * dw;
+    com.w += memDesc.getWeight(i);
+  }
+  return com;
+}// enf of centerOfMassCPU
+//----------------------------------------------------------------------------------------------------------------------
